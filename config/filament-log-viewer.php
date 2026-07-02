9802919203d28da87656d0ff9b31e36f0b42f4cc
NRotili <rotilinicolas@gmail.com>
2026-07-02 17:07:27 -0300

    fix: castear a int/bool los valores env del config de filament-log-viewer

--- config/filament-log-viewer.php
+++ config/filament-log-viewer.php
@@ -11,6 +11,6 @@ return [
     | The maximum size (in kilobytes) for a single log file to be read.
     | Files larger than this will be skipped to prevent memory exhaustion.
     */
-    'max_log_file_size' => env('LOG_MAX_SIZE_KB', 2048),
-    'enable_delete' => env('LOG_ENABLE_DELETE', true),
+    'max_log_file_size' => (int) env('LOG_MAX_SIZE_KB', 2048),
+    'enable_delete' => (bool) env('LOG_ENABLE_DELETE', true),
 ];
